9c69838b6dc8aae2cccc93706cf3f4027665676c
Kiko Golden <94949765+KikoGolden@users.noreply.github.com>
2021-11-28 11:37:19 +0200

    v0.03
    Working comment likes added

--- insta/myscript.js
+++ insta/myscript.js
@@ -126,3 +126,89 @@ function toggleComments(){
     commentMenu.style.width = '0px';
     }
 }
+
+//------comment like reaction
+//1st
+let commentHeart = document.getElementById('comment-like');
+let commentHeartLiked = document.getElementById('comment-unlike');
+let allCommentLikes= document.getElementById('commentLikes')
+let commentLikesNumber = 4170;
+allCommentLikes.textContent = commentLikesNumber;
+
+function commLike(){
+  commentHeartLiked.classList.remove('unlike');
+  commentHeart.classList.add('like');
+
+  commentLikesNumber++;
+  allCommentLikes.textContent = commentLikesNumber;
+}
+function commUnlike(){
+    commentHeartLiked.classList.add('unlike');
+    commentHeart.classList.remove('like');
+    
+    commentLikesNumber--;
+    allCommentLikes.textContent = commentLikesNumber;
+}
+//2nd
+let commentHeart2 = document.getElementById('comment-like2');
+let commentHeartLiked2 = document.getElementById('comment-unlike2');
+let allCommentLikes2= document.getElementById('commentLikes2')
+let commentLikesNumber2 = 1202;
+allCommentLikes2.textContent = commentLikesNumber2;
+
+function commLike2(){
+  commentHeartLiked2.classList.remove('unlike');
+  commentHeart2.classList.add('like');
+
+  commentLikesNumber2++;
+  allCommentLikes2.textContent = commentLikesNumber2;
+}
+function commUnlike2(){
+    commentHeartLiked2.classList.add('unlike');
+    commentHeart2.classList.remove('like');
+    
+    commentLikesNumber2--;
+    allCommentLikes2.textContent = commentLikesNumber2;
+}
+//3rd
+let commentHeart3 = document.getElementById('comment-like3');
+let commentHeartLiked3 = document.getElementById('comment-unlike3');
+let allCommentLikes3= document.getElementById('commentLikes3')
+let commentLikesNumber3 = 1111;
+allCommentLikes3.textContent = commentLikesNumber3;
+
+function commLike3(){
+  commentHeartLiked3.classList.remove('unlike');
+  commentHeart3.classList.add('like');
+
+  commentLikesNumber3++;
+  allCommentLikes3.textContent = commentLikesNumber3;
+}
+function commUnlike3(){
+    commentHeartLiked3.classList.add('unlike');
+    commentHeart3.classList.remove('like');
+    
+    commentLikesNumber3--;
+    allCommentLikes3.textContent = commentLikesNumber3;
+}
+//4rd
+let commentHeart4 = document.getElementById('comment-like4');
+let commentHeartLiked4 = document.getElementById('comment-unlike4');
+let allCommentLikes4= document.getElementById('commentLikes4')
+let commentLikesNumber4 = 469;
+allCommentLikes4.textContent = commentLikesNumber4;
+
+function commLike4(){
+  commentHeartLiked4.classList.remove('unlike');
+  commentHeart4.classList.add('like');
+
+  commentLikesNumber4++;
+  allCommentLikes4.textContent = commentLikesNumber4;
+}
+function commUnlike4(){
+    commentHeartLiked4.classList.add('unlike');
+    commentHeart4.classList.remove('like');
+    
+    commentLikesNumber4--;
+    allCommentLikes4.textContent = commentLikesNumber4;
+}
